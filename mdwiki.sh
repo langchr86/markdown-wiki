85ea948195392325599ea62e59ac10690ac018b0
Christian Lang <lang.chr86@gmail.com>
2017-01-01 18:42:50 +0100

    * Change to allow recursive iteration trough sub-folders and rework whole script.

--- mdwiki.sh
+++ mdwiki.sh
@@ -2,19 +2,24 @@
 
 # Tool to generate simple html wiki from a structure of markdown files. It uses only html and CSS.
 #
-# Only one sub-folder level is supported. All markdown files need to be in a subfolder.
-# All other files are simply copied to destination folder.
+# Multiple levels of sub-folders are supported. Each folder (also the root) needs an index.md file
+# to be processed. All md files needs a title section at top, like: "% test-title"
+# All other files then markdown are simply copied to destination folder.
+#
 # Example file structure:
 # wiki
+# |- index.md
 # |- tools
+# |  |- index.md
 # |  |- tmux.md
 # |  +- bash.md
 # |- others
+# |  |- index.md
 #    |- dos.md
 #    +- example.png
 #
 # To generate the Webpage in the folder /tmp/page if the source wiki folder is on /tmp/wiki you call the script as follows:
-# $ mdwiki.sh /tmp/wiki /tmp/page "ExampleWiki" "This is the description"
+# $ mdwiki.sh /tmp/wiki /tmp/page
 #
 # Requirements: pandoc
 
@@ -24,102 +29,116 @@
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 
-if [ $# -ne 4 ]; then
-	echo "usage: $0 SOURCE_FOLDER DESTINATION_FOLDER TITLE DESCRIPTION"
+if [[ $# -ne 2 && $# -ne 3 ]]; then
+	echo "usage: $0 SOURCE_FOLDER DESTINATION_FOLDER NOTTOPLEVEL"
+	echo "       NOTTOPLEVEL marks a not top level folder it is not an empty string."
 	exit 1
 fi
 
+
 SOURCE_FOLDER=$1
 DESTINATION_FOLDER=$2
-TITLE=$3
-DESCRIPTION=$4
-MARKDOWN="pandoc --from markdown --to html5 --section-divs --mathml --toc --toc-depth=1 -c ../style.css --template=${SCRIPT_DIR}/template.html"
+NOTTOPLEVEL=$3
+TMP_INDEX=${DESTINATION_FOLDER}/index.md
+MARKDOWN="pandoc --from markdown --to html5 --section-divs --mathml --toc --toc-depth=1 -c ./style.css --template=${SCRIPT_DIR}/template.html"
+
+
+# check that pandoc is installed
+hash pandoc 2>/dev/null || { echo >&2 "I require pandoc but it's not installed.  Aborting."; exit 1; }
 
 
 # empty destination folder
-echo "empty destination: ${DESTINATION_FOLDER}"
+mkdir -p ${DESTINATION_FOLDER}
 rm -rf ${DESTINATION_FOLDER}/*
 
-# prepare index file
-INDEX_HTML=${DESTINATION_FOLDER}/index.html
-printf "<!DOCTYPE HTML>\n" >> ${INDEX_HTML}
-printf "<html>\n" >> ${INDEX_HTML}
-printf "<head>\n" >> ${INDEX_HTML}
-printf "<title>${TITLE}</title>\n" >> ${INDEX_HTML}
-printf "<meta http-equiv="content-type" content="charset=utf-8">\n" >> ${INDEX_HTML}
-printf "</head>\n" >> ${INDEX_HTML}
-printf "<link rel="stylesheet" type="text/css" href="style.css">\n" >> ${INDEX_HTML}
-printf "<body>\n" >> ${INDEX_HTML}
-printf "<div id="content">\n" >> ${INDEX_HTML}
-printf "<h1>${TITLE}</h1>\n" >> ${INDEX_HTML}
-printf "<p>${DESCRIPTION}</p>\n" >> ${INDEX_HTML}
+# check if index file available
+if [ ! -f ${SOURCE_FOLDER}/index.md ]; then
+	echo "no index.md in ${SOURCE_FOLDER}"
+	exit 1
+fi
+
+# prepare temporary index file for content links if any md files
+cp ${SOURCE_FOLDER}/index.md ${TMP_INDEX}
+num_md=`ls -al ${SOURCE_FOLDER}/*.md | grep -c md`
+if [ $num_md -gt 1 ]; then
+	printf "\n\n# Articles\n\n" >> ${TMP_INDEX}
+fi
 
 # start processing
-echo "start processing in: ${SOURCE_FOLDER}"
+echo "${SOURCE_FOLDER}"
 
-# iterate over subdirectories
-for dir in ${SOURCE_FOLDER}/*/
+# iterate over markdown files in subdirectory
+for file in ${SOURCE_FOLDER}/*.md
 do
-	# ignore folder without any md file
-	dir_pattern="${dir}/*.md"
-	if [ ! '$(ls -A "${dir_pattern}" 2>/dev/null)' ]; then
-	  echo "  skip ${dir}"
-	  continue
+	# extract file name without path
+	file=${file##*/}
+
+	# do not process index file
+	if [ ${file} == index.md ]; then
+		continue
 	fi
 
-	# get subdirectory
-    dir=${dir%*/}
-    dir=${dir##*/}
-	echo "  ${dir}"
+	# notify processed file
+	echo "  ${file}"
 
 	# prepare paths
-	SOURCE_SUBDIR=${SOURCE_FOLDER}/${dir}
-	DEST_SUBDIR=${DESTINATION_FOLDER}/${dir}
+	SOURCE_FILE=${SOURCE_FOLDER}/${file}
+	DEST_FILE=${DESTINATION_FOLDER}/${file%.*}.html
+	FILE_NAME=${file%.*}
+	FILE_LINK=./${FILE_NAME}.html
 
-	# make subdirectory in destination
-	mkdir ${DEST_SUBDIR}
+	# get file description of file title marked by starting %
+	FILE_DESC=$(sed -n -e 's/^.*% //p' ${SOURCE_FILE} | head -1)
 
-	# insert category in index file
-	printf "<h2>${dir}</h2>\n" >> ${INDEX_HTML}
-	printf "<ul>\n" >> ${INDEX_HTML}
+	# do processing
+	${MARKDOWN} --variable backtoindex ${SOURCE_FILE} >> ${DEST_FILE}
 
-	# iterate over markdown files in subdirectory
-	for file in ${SOURCE_SUBDIR}/*.md
-	do
-		file=${file##*/}
-		echo "    ${file}"
-
-		# prepare paths
-		SOURCE_FILE=${SOURCE_SUBDIR}/${file}
-		DEST_FILE=${DEST_SUBDIR}/${file%.*}.html
-		FILE_NAME=${file%.*}
-		FILE_LINK=${dir}/${FILE_NAME}.html
-
-		# get file description of first h1 title marked by === underline
-		FILE_DESC=`sed -e '/===*=/,$d' ${SOURCE_FILE}`
-
-		# do processing
-		${MARKDOWN} ${SOURCE_FILE} >> ${DEST_FILE}
-
-		# insert entry in index file
-		printf "<li><a href="${FILE_LINK}">${FILE_DESC}</a></li>\n" >> ${INDEX_HTML}
-	done
-
-	# copy all other files than md
-	find ${SOURCE_SUBDIR} -type f -not -name "*.md" -exec cp {} ${DEST_SUBDIR} \;
-
-	# close list
-	printf "</ul>\n" >> ${INDEX_HTML}
+	# insert entry in index file
+	printf "* [${FILE_DESC}](${FILE_LINK})\n" >> ${TMP_INDEX}
 done
 
-# close index file
-printf "</div>\n" >> ${INDEX_HTML}
-printf "</body>\n" >> ${INDEX_HTML}
-printf "</html>\n" >> ${INDEX_HTML}
+# copy all other files than .md
+find ${SOURCE_FOLDER} -maxdepth 1 -type f -not -name "*.md" -exec cp {} ${DESTINATION_FOLDER} \;
+
+# prepare index file for sub dir links if any
+num_child=`ls -al ${SOURCE_FOLDER} | grep -c ^d`
+if [ $num_child -gt 2 ]; then
+	printf "\n\n# Sub Articles\n\n" >> ${TMP_INDEX}
+fi
+
+# iterate over all sub directories with index file
+for sub in ${SOURCE_FOLDER}/*/
+do
+	# extract file name without path
+	sub=$(basename ${sub})
+	SOURCE_SUB=${SOURCE_FOLDER}/${sub}
+	DEST_SUB=${DESTINATION_FOLDER}/${sub}
+
+	# check if index file available
+	if [ ! -f ${SOURCE_SUB}/index.md ]; then
+		continue
+	fi
+
+	# get file description of file title marked by starting %
+	FILE_DESC=$(sed -n -e 's/^.*% //p' ${SOURCE_SUB}/index.md | head -1)
+
+	# insert entry in index file
+	printf "* [${FILE_DESC}](./${sub}/index.html)\n" >> ${TMP_INDEX}
+
+	# recursive process sub folders
+	$0 ${SOURCE_SUB} ${DEST_SUB} "ntl"
+done
+
+# define if this is the top level index which needs not back link
+TOPTOINDEX=""
+if [ ! -z ${NOTTOPLEVEL} ]; then
+	TOPTOINDEX="--variable totopindex"
+fi
+
+# process temporary index file
+${MARKDOWN} ${TOPTOINDEX} ${TMP_INDEX} >> ${DESTINATION_FOLDER}/index.html
+#rm ${TMP_INDEX}
 
 # copy in style
 cp ${SCRIPT_DIR}/style.css ${DESTINATION_FOLDER}
 cp ${SCRIPT_DIR}/back.png ${DESTINATION_FOLDER}
-
-
-
